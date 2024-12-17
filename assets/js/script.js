c55988537e33da9448a21c03b467dce25556c7b6
emilien <emilien.lechat--maublanc@lamanu-student.fr>
2024-12-17 22:50:15 +0100

    format document

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ scrollContainer.addEventListener("wheel", (evt) => {
     scrollContainer.scrollLeft += evt.deltaY;
 });
 
-let nav=document.querySelector(".buttonStock")
+let nav = document.querySelector(".buttonStock")
 
 const url = 'https://api.themoviedb.org/3/genre/movie/list?language=en';
 const options = {
@@ -16,105 +16,52 @@ const options = {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODc0M2Y4NGRhYzAwNDBlZjEyNjgwMjMzZWU5NmEwZiIsIm5iZiI6MTczNDM0NDk4NC44MTUsInN1YiI6IjY3NjAwMTE4NWJkM2M3MmE4MmMxYzNiYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._wZmMme3uyW9tlwonW8YiyvgMC7zZTBAn4VG7ESRAWo'
     }
 };
-fetch(url, options).then((response)=>{
+fetch(url, options).then((response) => {
     return response.json();
-}).then(data=>{
+}).then(data => {
     data.genres.forEach(element => {
-        if (element.name=="Action" || element.name=="Animation" || element.name=="Documentary" || element.name=="History" || element.name=="Horror" || element.name=="Science Fiction" || element.name=="War" || element.name=="Western"){
-            let button=document.createElement("button");
-            button.innerHTML=element.name;
+        if (element.name == "Action" || element.name == "Animation" || element.name == "Documentary" || element.name == "History" || element.name == "Horror" || element.name == "Science Fiction" || element.name == "War" || element.name == "Western") {
+            let button = document.createElement("button");
+            button.innerHTML = element.name;
             nav.appendChild(button);
             button.classList.add("autoButton");
             button.classList.add(String(element.name.split(' ').join('')))
-            let k=document.querySelector("."+element.name.split(' ').join(''))
-            k.addEventListener("click", function(){
+            let k = document.querySelector("." + element.name.split(' ').join(''))
+            k.addEventListener("click", function () {
                 scrollContainer.innerHTML = ""
-                imagesButtonChange("genre",element.id)
+                imagesButtonChange("genre", element.id)
             })
         }
     });
 });
 
-let mostPopular=document.querySelector(".mostPopular");
-let newest=document.querySelector(".newest");
-let buttons=document.querySelector(".buttonStock button");
+let mostPopular = document.querySelector(".mostPopular");
+let newest = document.querySelector(".newest");
+let buttons = document.querySelector(".buttonStock button");
 
-mostPopular.addEventListener("click", function(){
+mostPopular.addEventListener("click", function () {
     scrollContainer.innerHTML = ""
     imagesButtonChange("popular")
 })
-newest.addEventListener("click", function(){
+newest.addEventListener("click", function () {
     scrollContainer.innerHTML = ""
     imagesButtonChange("newest")
 })
 
-// animation.addEventListener("click", function(){
-//     scrollContainer.innerHTML = ""
-//     imagesButtonChange("Animation")
-// })
-// documentary.addEventListener("click", function(){
-//     scrollContainer.innerHTML = ""
-//     imagesButtonChange("Documentary")
-// })
-// history.addEventListener("click", function(){
-//     scrollContainer.innerHTML = ""
-//     imagesButtonChange("History")
-// })
-// horror.addEventListener("click", function(){
-//     scrollContainer.innerHTML = ""
-//     imagesButtonChange("Horror")
-// })
-// sciFi.addEventListener("click", function(){
-//     scrollContainer.innerHTML = ""
-//     imagesButtonChange("Science Fiction")
-// })
-// war.addEventListener("click", function(){
-//     scrollContainer.innerHTML = ""
-//     imagesButtonChange("War")
-// })
-// western.addEventListener("click", function(){
-//     scrollContainer.innerHTML = ""
-//     imagesButtonChange("Western")
-// });
-
-function imagesButtonChange(type, genreId=""){
+function imagesButtonChange(type, genreId = "") {
     console.log(type)
-    if (type==="popular"){
+    if (type === "popular") {
         callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc');
     }
-    else if (type==="newest"){
+    else if (type === "newest") {
         callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=primary_release_date.desc');
     }
-    else if (type==="genre"){
-        callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&with_genres='+String(genreId))
+    else if (type === "genre") {
+        callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&with_genres=' + String(genreId))
     }
-    // else if (type==="Action"){
-    //     callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&with_genres=28');
-    // }
-    // else if (type==="Animation"){
-    //     callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&with_genres=16');
-    // }
-    // else if (type==="Documentary"){
-    //     callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&with_genres=99');
-    // }
-    // else if (type==="History"){
-    //     callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&with_genres=36');
-    // }
-    // else if (type==="Horror"){
-    //     callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&with_genres=27');
-    // }
-    // else if (type==="Science Fiction"){
-    //     callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&with_genres=878');
-    // }
-    // else if (type==="War"){
-    //     callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&with_genres=10752');
-    // }
-    // else if (type==="Western"){
-    //     callAPI('https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&with_genres=37');
-    // }
 };
 
-function callAPI(siteUrl){
+function callAPI(siteUrl) {
     const url = siteUrl;
     const options = {
         method: 'GET',
@@ -123,15 +70,15 @@ function callAPI(siteUrl){
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODc0M2Y4NGRhYzAwNDBlZjEyNjgwMjMzZWU5NmEwZiIsIm5iZiI6MTczNDM0NDk4NC44MTUsInN1YiI6IjY3NjAwMTE4NWJkM2M3MmE4MmMxYzNiYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._wZmMme3uyW9tlwonW8YiyvgMC7zZTBAn4VG7ESRAWo'
         }
     };
-    fetch (url,options).then((response)=>{
+    fetch(url, options).then((response) => {
         return response.json();
-    }).then(data=>{
+    }).then(data => {
         data.results.forEach(element => {
             if (element.poster_path != null) {
                 let title = element.original_title;
                 let poster = element.poster_path;
-                let images= document.createElement("img");
-                images.src= `https://images.tmdb.org/t/p/w500/${poster}`;
+                let images = document.createElement("img");
+                images.src = `https://images.tmdb.org/t/p/w500/${poster}`;
                 scrollContainer.appendChild(images);
             }
         });

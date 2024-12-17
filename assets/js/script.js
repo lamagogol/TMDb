ad1fde09b259cf138a5d86302f7e8768d26799a7
emilien <emilien.lechat--maublanc@lamanu-student.fr>
2024-12-17 09:18:38 +0100

    added api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+const url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODc0M2Y4NGRhYzAwNDBlZjEyNjgwMjMzZWU5NmEwZiIsIm5iZiI6MTczNDM0NDk4NC44MTUsInN1YiI6IjY3NjAwMTE4NWJkM2M3MmE4MmMxYzNiYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._wZmMme3uyW9tlwonW8YiyvgMC7zZTBAn4VG7ESRAWo'
+    }
+};
+
 const scrollContainer = document.querySelector(".scrollContainer");
 
 scrollContainer.addEventListener("wheel", (evt) => {

b8c66518a9add14cc6ce0f92fccb0a63805f9e72
emilien <emilien.lechat--maublanc@lamanu-student.fr>
2024-12-18 19:27:55 +0100

    cleaned js

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,7 +79,6 @@ function callAPI(siteUrl, genreId) {
                 let images = document.createElement("img");
                 images.src = `https://images.tmdb.org/t/p/w500/${poster}`;
                 images.classList.add(String(id));
-                console.log(id)
                 scrollContainer.appendChild(images);
                 images.addEventListener("click", function () {
                     openPage(id)
@@ -110,7 +109,6 @@ search.addEventListener("input", research)
 function research(e) {
     let query = e.target.value
     const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}&include_adult=false&language=en-US&page=1`;
-
     fetch(searchUrl, {
         method: 'GET',
         headers: {
@@ -118,30 +116,28 @@ function research(e) {
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODc0M2Y4NGRhYzAwNDBlZjEyNjgwMjMzZWU5NmEwZiIsIm5iZiI6MTczNDM0NDk4NC44MTUsInN1YiI6IjY3NjAwMTE4NWJkM2M3MmE4MmMxYzNiYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._wZmMme3uyW9tlwonW8YiyvgMC7zZTBAn4VG7ESRAWo'
         }
     }).then((res) => res.json())
-    .then((data) => {
-        let main = document.querySelector(".main");
-        resultContainer.innerHTML = "";
-        main.style.display = "none";
-        resultContainer.style.display = "flex";
-        data.results.forEach(movie => {
-            if (movie.poster_path != null) {
-                console.log(movie);
-                let path = movie.poster_path
-                let images = document.createElement('img')
-                images.src = `https://images.tmdb.org/t/p/w500/${path}`;
-                resultContainer.appendChild(images);
-            }
-        })
-        if (query === "") {
-            main.style.display = "block";
-            resultContainer.style.display = "none"
+        .then((data) => {
+            let main = document.querySelector(".main");
             resultContainer.innerHTML = "";
-            return;
-        }
-        resultContainer.addEventListener("wheel", (evt) => {
-            evt.preventDefault();
-            resultContainer.scrollLeft += evt.deltaY;
-        });
-
-    })
+            main.style.display = "none";
+            resultContainer.style.display = "flex";
+            data.results.forEach(movie => {
+                if (movie.poster_path != null) {
+                    let path = movie.poster_path
+                    let images = document.createElement('img')
+                    images.src = `https://images.tmdb.org/t/p/w500/${path}`;
+                    resultContainer.appendChild(images);
+                }
+            })
+            if (query === "") {
+                main.style.display = "block";
+                resultContainer.style.display = "none"
+                resultContainer.innerHTML = "";
+                return;
+            }
+            resultContainer.addEventListener("wheel", (evt) => {
+                evt.preventDefault();
+                resultContainer.scrollLeft += evt.deltaY;
+            });
+        })
 }

175f5a70071c667d69bb192146604ff7b58a06fc
emilien <emilien.lechat--maublanc@lamanu-student.fr>
2024-12-18 19:32:41 +0100

    added rederect to producer site when searchbar film is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,28 +116,32 @@ function research(e) {
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ODc0M2Y4NGRhYzAwNDBlZjEyNjgwMjMzZWU5NmEwZiIsIm5iZiI6MTczNDM0NDk4NC44MTUsInN1YiI6IjY3NjAwMTE4NWJkM2M3MmE4MmMxYzNiYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._wZmMme3uyW9tlwonW8YiyvgMC7zZTBAn4VG7ESRAWo'
         }
     }).then((res) => res.json())
-        .then((data) => {
-            let main = document.querySelector(".main");
-            resultContainer.innerHTML = "";
-            main.style.display = "none";
-            resultContainer.style.display = "flex";
-            data.results.forEach(movie => {
-                if (movie.poster_path != null) {
-                    let path = movie.poster_path
-                    let images = document.createElement('img')
-                    images.src = `https://images.tmdb.org/t/p/w500/${path}`;
-                    resultContainer.appendChild(images);
-                }
-            })
-            if (query === "") {
-                main.style.display = "block";
-                resultContainer.style.display = "none"
-                resultContainer.innerHTML = "";
-                return;
+    .then((data) => {
+        let main = document.querySelector(".main");
+        resultContainer.innerHTML = "";
+        main.style.display = "none";
+        resultContainer.style.display = "flex";
+        data.results.forEach(movie => {
+            if (movie.poster_path != null) {
+                let path = movie.poster_path
+                let id=movie.id
+                let images = document.createElement('img')
+                images.src = `https://images.tmdb.org/t/p/w500/${path}`;
+                resultContainer.appendChild(images);
+                images.addEventListener("click", function () {
+                    openPage(id)
+                })
             }
-            resultContainer.addEventListener("wheel", (evt) => {
-                evt.preventDefault();
-                resultContainer.scrollLeft += evt.deltaY;
-            });
         })
+        if (query === "") {
+            main.style.display = "block";
+            resultContainer.style.display = "none"
+            resultContainer.innerHTML = "";
+            return;
+        }
+        resultContainer.addEventListener("wheel", (evt) => {
+            evt.preventDefault();
+            resultContainer.scrollLeft += evt.deltaY;
+        });
+    })
 }
